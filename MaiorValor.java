9665ac51d715a46ecf2f78593e21e0b39b704098
Gustavo Jacob <gustavo_jacob@localhost.localdomain>
2020-04-22 21:05:54 -0300

    add ex 1, 2 e 3

---
+++ MaiorValor.java
@@ -0,0 +1,30 @@
+
+public class MaiorValor {
+
+    public static void main(String[] args) {
+
+        int[] numbers = new int[10];
+        for (int i = 0; i < 10; i++) {
+            numbers[i] = (int) (1 + (Math.random() * 100));
+
+        }
+
+        for (int i = 0; i < 9; i++)
+
+            System.out.println(numbers[i]);
+
+        maxVal1(numbers, numbers.length);
+
+    }
+
+    public static int maxVal1(int A[], int n) {
+        int max = A[0];
+        for (int i = 1; i < n; i++) {
+            if (A[i] > max)
+                max = A[i];
+        }
+        System.out.println("O maior numero é: "+max);
+        return max;
+    }
+
+}
